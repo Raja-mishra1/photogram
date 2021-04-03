4d3b416655ca53e126d55377c13198d2df3053a2
Mishra-raja <rajamishra0702@gmail.com>
2021-04-03 11:11:31 +0530

    added post app

---
+++ backend/app/post/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from post import views
+
+
+router = DefaultRouter()
+router.register('', views.PostViewSet,basename='Core')
+
+app_name = 'post'
+
+urlpatterns = [
+    path('feed/',
+         views.UserFeedView.as_view(),
+         name='feed'),
+    path('', include(router.urls)),
+    path('comment/<uuid:post_id>/',
+         views.AddCommentView.as_view(),
+         name='add-comment'),
+    path('comment/<int:comment_id>/',
+         views.ManageCommentView.as_view(),
+         name='manage-comment'),
+    path('like/<uuid:post_id>/',
+         views.LikeView.as_view(),
+         name='like'),
+    path('<uuid:post_id>/get-likers/',
+         views.GetLikersView.as_view(),
+         name='get-likers'),
+]
---
+++ backend/app/post/views.py
@@ -0,0 +1,87 @@
+from rest_framework import permissions,\
+    viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import PostSerializer, CommentSerializer, AuthorSerializer
+from core.models import Post, Comment
+from .permissions import IsOwnerOrReadOnly, IsOwnerOrPostOwnerOrReadOnly
+from core.pagination import FollowersLikersPagination
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    permissions_classes = (
+         IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class AddCommentView(generics.CreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def post(self, request, post_id=None):
+        post = Post.objects.get(pk=post_id)
+        serializer = CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(post=post, author=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ManageCommentView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CommentSerializer
+    lookup_url_kwarg = 'comment_id'
+    permission_classes = (IsOwnerOrPostOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        return queryset
+
+
+
+class LikeView(APIView):
+    """Toggle like"""
+
+    def get(self, request, format=None, post_id=None):
+        post = Post.objects.get(pk=post_id)
+        user = self.request.user
+        if user.is_authenticated:
+            if user in post.likes.all():
+                like = False
+                post.likes.remove(user)
+            else:
+                like = True
+                post.likes.add(user)
+        data = {
+            'like': like
+        }
+        return Response(data)
+
+
+class GetLikersView(generics.ListAPIView):
+    serializer_class = AuthorSerializer
+    pagination_class = FollowersLikersPagination
+    permission_classes = (permissions.AllowAny,)
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        queryset = Post.objects.get(
+            pk=post_id).likes.all()
+        return queryset
+
+
+class UserFeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        queryset = Post.objects.all().filter(author__in=following_users)
+        return queryset
